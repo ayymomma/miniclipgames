0cdad7c0e3da8a0405cdb46f45df54bb62c650d1
Mircea Mihai Adrian <62558071+ayymomma@users.noreply.github.com>
2022-03-16 15:34:35 +0200

    frame for tictactoe

---
+++ customWidgets/games/ticTacToe/ticTacToeFrame.py
@@ -0,0 +1,11 @@
+from PyQt5.QtWidgets import QFrame
+
+
+class TicTacToeFrame(QFrame):
+    def __init__(self, parent):
+        super(TicTacToeFrame).__init__(parent)
+
+        self.setupUi()
+
+    def setupUi(self):
+        pass
